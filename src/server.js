5174dc22599819f4496e8cb8ed47112d34cccf98
JOHN NASIMBA <nasimba4john@gmail.com>
2020-10-04 21:52:13 +0200

    added post route for adding comments in sever.js using some fake data

--- src/server.js
+++ src/server.js
@@ -3,13 +3,16 @@ const bodyParser = require('body-parser')
 
 const articleInfo = {
     'learn-react': {
-        upvotes: 0
+        upvotes: 0,
+        comments: []
     },
     'learn-node': {
-        upvotes: 0
+        upvotes: 0,
+        comments:[]
     },
     'my-thoughts-on-resumes': {
-        upvotes:0
+        upvotes: 0,
+        comments: []
     }
 }
 
@@ -22,6 +25,13 @@ app.post('/api/articles/:name/upvote', (req, res) => {
     const articleName = req.params.name
     const newUpvotes = articleInfo[articleName].upvotes += 1;
     res.status(200).send(`${articleName} now has ${newUpvotes} upvotes`)
+})
+app.post('/api/articles/:name/add-comment', (req, res) => {
+    const { name, text } = req.body
+    const articleName = req.params.name
+    articleInfo[articleName].comments.push({ name, text });
+    res.status(200).send(articleInfo[articleName])
+
 })
 
 
