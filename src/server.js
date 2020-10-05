da0933fae6a3cd9a7539e5da91322df63cf2b65f
JOHN NASIMBA <nasimba4john@gmail.com>
2020-10-05 13:22:32 +0200

    Imported mongodb and created article route for getting a single article from the server best on url parameter

--- src/server.js
+++ src/server.js
@@ -1,26 +1,53 @@
 const express = require('express');
 const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient;
 
-const articleInfo = {
-    'learn-react': {
-        upvotes: 0,
-        comments: []
-    },
-    'learn-node': {
-        upvotes: 0,
-        comments:[]
-    },
-    'my-thoughts-on-resumes': {
-        upvotes: 0,
-        comments: []
-    }
-}
+
+
+/* const url = "mongodb+srv://JOHN:0995816060@cluster0.bfy6i.mongodb.net/Articles?retryWrites=true&w=majority";
+
+
+MongoClient.connect(url,{ useNewUrlParser: true, useUnifiedTopology: true }, function(err, db) {
+    if (err) throw err;
+    console.log("you are connected to the database");
+    const query = db.db('Articles')
+    query.collection("articleList").find({}).toArray( (err, result)=> {
+        if (err) throw err;
+        console.log(result);
+        db.close();
+    })
+   
+  }); */
+
+
+
 
 
 
 const app = express();
 const PORT = 8000;
 app.use(bodyParser.json())
+
+
+app.get('/api/articles/:name', async (req, res) => {
+    try {
+    const url = "mongodb+srv://JOHN:0995816060@cluster0.bfy6i.mongodb.net/Articles?retryWrites=true&w=majority";
+
+    const articleName = req.params.name
+    const client = await MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+
+        
+    const query = client.db('Articles')
+    const articleInfo = await query.collection("articleList").findOne({ name: articleName })
+    res.status(200).json(articleInfo);
+    client.close();  
+    } catch (error) {
+        res.status(500).json({message: 'Error connecting to db', error})
+    }
+        
+
+})
+
 app.post('/api/articles/:name/upvote', (req, res) => {
     const articleName = req.params.name
     const newUpvotes = articleInfo[articleName].upvotes += 1;
@@ -32,7 +59,7 @@ app.post('/api/articles/:name/add-comment', (req, res) => {
     articleInfo[articleName].comments.push({ name, text });
     res.status(200).send(articleInfo[articleName])
 
-})
+}) 
 
 
 app.listen(PORT, () => {
